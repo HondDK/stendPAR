fbe85d02043cc7f7a9c782291976840cce69de02
HondDK <78250871+HondDK@users.noreply.github.com>
2022-06-22 12:47:26 +0600

    Update stendPAR.ino

--- stendPAR.ino
+++ stendPAR.ino
@@ -9,38 +9,38 @@ const byte relayPin4 = 6; // паронагреватель (красный)
 
 void setup() {
  
- pinMode(button, INPUT);
+ pinMode(button, INPUT); // назначение кнопки (контакты)
  
- pinMode(relayPin1, OUTPUT);   
- pinMode(relayPin2, OUTPUT);  
- pinMode(relayPin3, OUTPUT);   
- pinMode(relayPin4, OUTPUT); 
+ pinMode(relayPin1, OUTPUT);  // назначение реле  
+ pinMode(relayPin2, OUTPUT);  // назначение реле  
+ pinMode(relayPin3, OUTPUT);  // назначение реле  
+ pinMode(relayPin4, OUTPUT);  // назначение реле  
  
- digitalWrite(relayPin1, HIGH);
- digitalWrite(relayPin2, HIGH); 
- digitalWrite(relayPin3, HIGH); 
- digitalWrite(relayPin4, HIGH); 
+ digitalWrite(relayPin1, HIGH); // реле в выключенное состояние при включении ардуино
+ digitalWrite(relayPin2, HIGH); // реле в выключенное состояние при включении ардуино
+ digitalWrite(relayPin3, HIGH); // реле в выключенное состояние при включении ардуино
+ digitalWrite(relayPin4, HIGH); // реле в выключенное состояние при включении ардуино
  
 
 }
 
 void loop() {
-  if (digitalRead(button) == LOW ) {
-  digitalWrite(relayPin1, LOW); 
+  if (digitalRead(button) == LOW ) { // при замыкании контактов запускаются таймеры с включением лампочек по очереди
+  digitalWrite(relayPin1, LOW); //включение синей лампы
   delay(30000);
-  digitalWrite(relayPin2, LOW); 
+  digitalWrite(relayPin2, LOW); //включение зеленой лампы
   delay(20000);
-  digitalWrite(relayPin3, LOW); 
+  digitalWrite(relayPin3, LOW); //желтая лампа
   delay(30000);
-  digitalWrite(relayPin4, LOW); 
+  digitalWrite(relayPin4, LOW); //красная лампа
   delay(10000);
-  digitalWrite(relayPin1, HIGH); 
-  digitalWrite(relayPin2, HIGH); 
-  digitalWrite(relayPin3, HIGH); 
-  digitalWrite(relayPin4, HIGH); 
+  digitalWrite(relayPin1, HIGH); // выключение ламп после прохождение всех таймеров
+  digitalWrite(relayPin2, HIGH); // выключение ламп после прохождение всех таймеров
+  digitalWrite(relayPin3, HIGH); // выключение ламп после прохождение всех таймеров
+  digitalWrite(relayPin4, HIGH); // выключение ламп после прохождение всех таймеров
   
 }
-else {
+else {   // выключение ламп если контакты разомкнуты
   digitalWrite(relayPin1, HIGH); 
   digitalWrite(relayPin2, HIGH); 
   digitalWrite(relayPin3, HIGH); 
